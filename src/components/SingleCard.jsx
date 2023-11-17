548f508070c585e5d49d659143e724b4e234d156
realsingeriu <147463909+realsingeriu@users.noreply.github.com>
2023-11-17 10:33:58 +0900

    카드뒷면 클릭시 콘솔표시

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './SingleCard.css';
 
-const SingleCard = (props) => {
+const SingleCard = ({card}) => {
+  // 클릭 이벤트 핸들러 함수 정의
+  function handleClick() {
+    console.log(card) // 카드 정보를 콘솔에 출력
+  }
   return (
     <div className='card'>
       <div>
         {/* 카드 앞면 */}
-        <img className='front' src={props.card.src} alt='card front'></img>
-        {/* 카드 뒷면 */}
-        <img className='back' src='/img/cover.png' alt='card back'></img>
+        <img className='front' src={card.src} alt='card front'></img>
+        {/* 카드 뒷면, 클릭 이벤트에 handleClick 함수 연결 */}
+        <img onClick={handleClick} className='back' src='/img/cover.png' alt='card back'></img>
       </div>
     </div>
   );
